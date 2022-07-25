98f6259af4c04a65cb72ae0b6d9383035857b817
harudagondi <giogdeasis@gmail.com>
2022-07-25 20:44:18 +0800

    Fix mp3 loader

--- src/loader/mp3_loader.rs
+++ src/loader/mp3_loader.rs
@@ -13,17 +13,17 @@ impl AssetLoader for Mp3Loader {
         load_context: &'a mut LoadContext,
     ) -> BoxedFuture<'a, Result<(), Error>> {
         Box::pin(async move {
-            let decoder = Decoder::new(bytes);
+            let mut decoder = Decoder::new(bytes);
 
-            let samples = Vec::new();
+            let mut samples = Vec::new();
 
-            let mut sample_rate;
+            let mut current_sample_rate = 44100;
 
             loop {
                 match decoder.next_frame() {
                     Ok(minimp3::Frame {
-                        data, sample_rate, ..
-                    }) => samples.push([data[0], data[1]]),
+                        data, sample_rate , ..
+                    }) => {samples.push([data[0], data[1]]); current_sample_rate = sample_rate;},
                     Err(minimp3::Error::Eof) => break,
                     Err(err) => return Err(err.into()),
                 }
@@ -31,18 +31,19 @@ impl AssetLoader for Mp3Loader {
 
             let convert_i32_to_f32 = |int| {
                 if int < 0 {
-                    (int as f32) / -(i16::MIN as f32)
+                    (f32::from(int)) / -(f32::from(i16::MIN))
                 } else {
-                    (int as f32) / (i16::MAX as f32)
+                    (f32::from(int)) / f32::from(i16::MAX)
                 }
             };
 
-            let mut samples: Vec<[f32; 2]> = samples
+            let samples: Vec<[f32; 2]> = samples
                 .into_iter()
                 .map(|[l, r]| [convert_i32_to_f32(l), convert_i32_to_f32(r)])
                 .collect();
 
-            let frames = oddio::Frames::from_iter(sample_rate, samples);
+            #[allow(clippy::cast_sign_loss)]
+            let frames = oddio::Frames::from_iter(current_sample_rate as u32, samples);
 
             let audio_source = AudioSource { frames };
 
