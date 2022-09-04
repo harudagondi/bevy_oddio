2089ff6a66cd9f793e678468e0426d454644e1f8
harudagondi <giogdeasis@gmail.com>
2022-09-04 09:58:11 +0800

    Add support for 1-2 channels in loaders (#28)
    # Objective
    
    - partially do #4
    
    ## Solution
    
    - Add support for mono in
    	- flac
    	- wav
    	- ogg

--- src/loader/flac_loader.rs
+++ src/loader/flac_loader.rs
@@ -1,7 +1,10 @@
 use bevy::asset::{AssetLoader, BoxedFuture, Error, LoadContext, LoadedAsset};
 use claxon::FlacReader;
 
-use crate::{frames::Stereo, AudioSource};
+use crate::{
+    frames::{Mono, Stereo},
+    AudioSource,
+};
 
 #[derive(Default)]
 pub struct FlacLoader;
@@ -15,6 +18,8 @@ impl AssetLoader for FlacLoader {
         #[allow(clippy::cast_precision_loss)]
         Box::pin(async move {
             let mut reader = FlacReader::new(bytes)?;
+
+            let channels = reader.streaminfo().channels;
 
             let sample_rate = reader.streaminfo().sample_rate;
 
@@ -30,16 +35,31 @@ impl AssetLoader for FlacLoader {
 
             let mut samples: Vec<f32> = samples.into_iter().map(convert_i32_to_f32).collect();
 
-            let frames = oddio::Frames::from_iter(
-                sample_rate,
-                oddio::frame_stereo(&mut samples)
-                    .iter_mut()
-                    .map(|frame| Stereo::from(*frame)),
-            );
+            match channels {
+                1 => {
+                    let frames = oddio::Frames::from_iter(
+                        sample_rate,
+                        samples.into_iter().map(|frame| Mono::from([frame])),
+                    );
 
-            let audio_source = AudioSource { frames };
+                    let audio_source = AudioSource { frames };
 
-            load_context.set_default_asset(LoadedAsset::new(audio_source));
+                    load_context.set_default_asset(LoadedAsset::new(audio_source));
+                }
+                2 => {
+                    let frames = oddio::Frames::from_iter(
+                        sample_rate,
+                        oddio::frame_stereo(&mut samples)
+                            .iter_mut()
+                            .map(|frame| Stereo::from(*frame)),
+                    );
+
+                    let audio_source = AudioSource { frames };
+
+                    load_context.set_default_asset(LoadedAsset::new(audio_source));
+                }
+                _ => unimplemented!("bevy_oddio only have support for 1 or 2 channels only."),
+            }
 
             Ok(())
         })
